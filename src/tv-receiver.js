0342b67d82a6f556f3cb5262993f50772fc1bddf
nemesis15 <rafael.prevost.15@gmail.com>
2016-05-22 16:20:34 -0400

    fuck tha shit

--- src/tv-receiver.js
+++ src/tv-receiver.js
@@ -1,84 +1,122 @@
 function creerVertexTeleRecepteur(objgl, fltLargeur, fltProfondeur) {
-    
+   var objVertex = objgl.createBuffer();
+
     if(!aerial) {
-        var tabVertex = [0.5,-0.9,0.5] // Le centre du cercle;
-           for (var i = 0; i <= 360; i++) {
-             tabVertex = tabVertex.concat([0.5+Math.cos(i*Math.PI/180)*0.5,-0.9,0.5+Math.sin(i*Math.PI/180)*0.5]);
-         }
-    
-         tabVertex = tabVertex.concat([0.5,height-1,0.5]) // Le centre du cercle;
-           for (var i = 0; i <= 360; i++) {
-             tabVertex = tabVertex.concat([0.5+Math.cos(i*Math.PI/180)*0.5,height-1.5,0.5+Math.sin(i*Math.PI/180)*0.5]);
-         }
+        tabVertex = 
+        [
+            // Bas
+            0.75, -1.0, 0.25,// 7
+            0.75, -1.0, 0.75, //4 
+            0.25, -1.0, 0.75, // 0
+            0.25, -1.0, 0.25, //3 
+            
+            // Haut
+            0.75, 1.0, 0.25,// 7
+            0.75, 1.0, 0.75, //4 
+            0.25, 1.0, 0.75, // 0
+            0.25, 1.0, 0.25, //3 
+        ];
     } else {
-       var tabVertex = [0.5,0,0.5] // Le centre du cercle;
-       for (var i = 0; i <= 360; i++) {
-         tabVertex = tabVertex.concat([0.5+Math.cos(i*Math.PI/180)*0.5,-0.9,0.5+Math.sin(i*Math.PI/180)*0.5]);
-       }
+        tabVertex = 
+        [
+            0.75, 0.0, 0.25,// 7
+            0.75, 0.0, 0.75, //4 
+            0.25, 0.0, 0.75, // 0
+            0.25, 0.0, 0.25, //3 
+        ]
     }
-    
-    var objVertexTeleTransporteur = objgl.createBuffer();
-    objgl.bindBuffer(objgl.ARRAY_BUFFER, objVertexTeleTransporteur);
+
+    objgl.bindBuffer(objgl.ARRAY_BUFFER, objVertex);
     objgl.bufferData(objgl.ARRAY_BUFFER, new Float32Array(tabVertex), objgl.STATIC_DRAW);
-    objVertexTeleTransporteur.intNbElems = tabVertex.length/3; objVertexTeleTransporteur.intTailleElem = 3;
-    return objVertexTeleTransporteur;
+    objVertex.intNbElems = tabVertex.length/3; objVertex.intTailleElem = 3;
+   
+   return objVertex;   
 }
 
 function creerCouleursTeleRecepteur(objgl) {
-    tabCouleurs = []; 
-    for (var i = 0; i <= 361; i++)
-        tabCouleurs = tabCouleurs.concat([0.0,0.0,1.0,0.0]);
-    
-    if(!aerial) {  
-        for (var i = 0; i <= 361; i++)
-            tabCouleurs = tabCouleurs.concat([0.0,0.0,1.0,0.0]);
-    }
+          var objCouleursCube = objgl.createBuffer();
 
-    var objCouleursTeleTransporteur = objgl.createBuffer();
-    objgl.bindBuffer(objgl.ARRAY_BUFFER, objCouleursTeleTransporteur);
-    objgl.bufferData(objgl.ARRAY_BUFFER, new Float32Array(tabCouleurs), objgl.STATIC_DRAW);
-    objCouleursTeleTransporteur.intNbElems = tabCouleurs.length; objCouleursTeleTransporteur.intTailleElem = 4;
-    return objCouleursTeleTransporteur;
+        if(!aerial) {
+            tabCouleurs = []
+            for(var i = 0; i< 8;i++)
+                tabCouleurs = tabCouleurs.concat([1.0, 1.0, 0.0, 1.0]);
+        } else {
+            tabCouleurs = []
+            for(var i = 0; i< 4;i++)
+                tabCouleurs = tabCouleurs.concat([1.0, 1.0, 0.0, 1.0]);
+        }
+
+        objgl.bindBuffer(objgl.ARRAY_BUFFER, objCouleursCube);
+        objgl.bufferData(objgl.ARRAY_BUFFER, new Float32Array(tabCouleurs), objgl.STATIC_DRAW);
+
+        objCouleursCube.intNbElems = tabCouleurs.length/4; objCouleursCube.intTailleElem = 4;
+
+        return objCouleursCube;
 }
 
-function creerTexelsTeleRecpteur(objgl, fltLargeur, fltProfondeur) {
-     var tabTexels = [] // Le centre du cercle;
-       for (var i = 0; i <= 361; i++) {
-         tabTexels = tabTexels.concat([0.0,0.0]);
-     }
-     
-       for (var i = 0; i <= 361; i++) {
-         tabTexels = tabTexels.concat([0.0,0.0]);
-     }
-    
-    var objTexelsTeleTransporteur = objgl.createBuffer();
-    objgl.bindBuffer(objgl.ARRAY_BUFFER, objTexelsTeleTransporteur);
-    objgl.bufferData(objgl.ARRAY_BUFFER, new Float32Array(tabTexels), objgl.STATIC_DRAW);
-    objTexelsTeleTransporteur.intNbElems = tabTexels.length/2; objTexelsTeleTransporteur.intTailleElem = 2;
-    objTexelsTeleTransporteur.intNoTexture = TEX_PLANCHER; objTexelsTeleTransporteur.pcCouleurTexel = 0;
-    
-    return objTexelsTeleTransporteur;
+function creerTexelsTeleRecepteur(objgl, fltLargeur, fltProfondeur) {
+      var objTexelsCube = objgl.createBuffer();
+
+        if(!aerial) {
+            tabTexels = [  // Texels de la face avant
+                          1.0, 0.0,  // 1: Coin haut droit
+                          1.0, 1.0,  // 2: Coin bas droit
+                          0.0, 1.0,  // 3: Coin bas gauche
+                          0.0, 0.0,  // 4: Coin haut gauche
+
+                          // Texels de la face arrière
+                          1.0, 0.0,   // 6: Coin haut droit
+                          1.0, 1.0,   // 7: Coin bas droit
+                          0.0, 1.0,   // 8: Coin bas gauche
+                          0.0, 0.0    // 9: Coin haut gauche
+            ];
+        } else {
+             tabTexels = [  // Texels de la face avant
+                          1.0, 0.0,  // 1: Coin haut droit
+                          1.0, 1.0,  // 2: Coin bas droit
+                          0.0, 1.0,  // 3: Coin bas gauche
+                          0.0, 0.0,  // 4: Coin haut gauche
+            ];
+        }
+
+        objgl.bindBuffer(objgl.ARRAY_BUFFER, objTexelsCube);
+        objgl.bufferData(objgl.ARRAY_BUFFER, new Float32Array(tabTexels), objgl.STATIC_DRAW);
+
+        // 10 texels
+        objTexelsCube.intNbElems = tabTexels.length/2; objTexelsCube.intTailleElem = 2;
+        // 100% de la texture est utilisée
+        objTexelsCube.intNoTexture = 1; objTexelsCube.pcCouleurTexel = 0.00;
+
+        return objTexelsCube;
   }
 
 function creerMaillageTeleRecepteur(objgl) {
-		var objMaillageCube = objgl.createBuffer();
-		
-		var tabMaillage = []
-		for(var i = 1; i <= 360; i++)  {
-		    tabMaillage = tabMaillage.concat([0,i,i+1]);
-		}
-		if(!aerial) {  
-    		for(var i = 362; i <= 360*2+2; i++)  {
-    		    tabMaillage = tabMaillage.concat([362,i,i+1]);
-    		}
-		}
-		
-	    var objMaillageTeleTransporteur = objgl.createBuffer();
-        objgl.bindBuffer(objgl.ELEMENT_ARRAY_BUFFER, objMaillageTeleTransporteur);
-        objgl.bufferData(objgl.ELEMENT_ARRAY_BUFFER, new Uint16Array(tabMaillage), objgl.STATIC_DRAW);
+    var objMaillageCube = objgl.createBuffer();
+        // Le maillage
+        
+        if(!aerial) {
+            tabMaillageCube =
+                [ // Les 4 triangles de la face avant
+                     3,2,0,
+          			 1,2,0,
+          			 7,6,4,
+          			 5,6,4
+                ];
+        } else {
+             tabMaillageCube =
+                [ // Les 4 triangles de la face avant
+                     3,2,0,
+          			 1,2,0
+                ];
+        }
 
-        objMaillageTeleTransporteur.intNbElemsTriangles = tabMaillage.length;
-        objMaillageTeleTransporteur.intNbElemsDroites = 0;
-		
-        return objMaillageTeleTransporteur;
+        objgl.bindBuffer(objgl.ELEMENT_ARRAY_BUFFER, objMaillageCube);
+        objgl.bufferData(objgl.ELEMENT_ARRAY_BUFFER, new Uint16Array(tabMaillageCube), objgl.STATIC_DRAW);
+
+        // Le nombre de vertex pour les triangles
+        objMaillageCube.intNbElemsTriangles = tabMaillageCube.length;
+        // Le nombre de vertex pour les droites
+        objMaillageCube.intNbElemsDroites = 0;
+
+        return objMaillageCube;
     }
